7700e37dc308a7269f8385f79061865faf105d67
Martin Yanchev <myanch200@gmail.com>
2022-01-11 01:22:29 +0000

    adding assosiations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ApplicationRecord
-    has_one :user, dependent: :delete_all
+    belongs_to :user
     validates :date, presence: true
     validates :hours, presence: true
     scope :from_this_month, lambda { where("entries.date > ? AND entries.date < ?", Time.now.beginning_of_month, Time.now.end_of_month) }
