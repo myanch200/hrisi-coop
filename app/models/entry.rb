3dd61375e75fb65c8b1ebae2611f5517b40c3c1e
Martin Yanchev <myanch200@gmail.com>
2022-01-11 01:13:56 +0000

    adding cascade

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,5 @@
 class Entry < ApplicationRecord
+    has_one :user, dependent: :delete_all
     validates :date, presence: true
     validates :hours, presence: true
     scope :from_this_month, lambda { where("entries.date > ? AND entries.date < ?", Time.now.beginning_of_month, Time.now.end_of_month) }
