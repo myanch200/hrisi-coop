11a4670622ff245fe3b0a3e8f7105de8132f0313
Martin Yanchev <myanch200@gmail.com>
2022-01-22 16:44:37 +0000

    add:adding page for not registered users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
+  
   def landing
-    @transaction = Transaction.new
-    @transactions = Transaction.by_current_user(current_user)
+    if current_user
+      @transaction = Transaction.new
+      @transactions = Transaction.by_current_user(current_user)
+    end
   end
 end
