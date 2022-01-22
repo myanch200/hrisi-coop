d579cff18e04d3ad9eb7454169dbf33ef84de953
Martin Yanchev <myanch200@gmail.com>
2022-01-22 03:58:12 +0000

    add: adding default value for note

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_034250) do
+ActiveRecord::Schema.define(version: 2022_01_22_035519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2022_01_22_034250) do
     t.bigint "user_id"
     t.decimal "amount", precision: 5, scale: 2, null: false
     t.string "transaction_type", null: false
-    t.text "note"
+    t.text "note", default: "No note"
     t.string "category", default: "general"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
